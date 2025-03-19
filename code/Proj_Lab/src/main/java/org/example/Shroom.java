bebf318262f9bfd8194c8d36d2f8fd7bb9642008
darksidekovi <kovidavid96@gmail.com>
2025-03-20 00:23:23 +0100

    add Shroom definition

--- code/Proj_Lab/src/main/java/org/example/Shroom.java
+++ code/Proj_Lab/src/main/java/org/example/Shroom.java
@@ -1,5 +1,46 @@
 package org.example;
 
 public class Shroom {
+    // private int team_id;
+    // private Tekton tekton;
+    // private int _age;
+    // private int lastEject;
 
+    public static void isOld() {
+        // return this._age > LIMIT;
+    }
+
+    public static void ejectSpore(Tekton T) { }
+
+    public static void isSporeReady() { }
+
+    public static void die() { }
+
+    public static void age() {
+        // _age++;
+    }
+
+    public static void getTeam_id() {
+        // return team_id;
+    }
+
+    public static void getTekton() {
+        // return tekton;
+    }
+
+    public static void getAge() {
+        // return _age;
+    }
+
+    public static void getLastEject() {
+        // return lastEject;
+    }
+
+    public static void setTeam_id(int id) {
+        // this.team_id = id;
+    }
+
+    public static void setTekton(Tekton T) {
+        // this.tekton = T;
+    }
 }
