b3be4c2edff57f1cffe29b76f117f5f9f76ddbff
Spertli Krisztián <spertlik13@gmail.com>
2025-03-19 21:45:11 +0100

    Player class skeleton

--- code/Proj_Lab/src/main/java/org/example/Player.java
+++ code/Proj_Lab/src/main/java/org/example/Player.java
@@ -1,5 +1,54 @@
 package org.example;
 
-public class Player {
+import java.util.List;
 
+public class Player {
+    //private int score;
+    //private boolean isInsect;
+    //private Tekton currentTekton;
+    //private int[] effects = new int[4];
+
+    public static void interactWithSpore(List<Spore> spores) {
+        // Implementation
+    }
+
+    public static void interactWithYarn(Yarn yarn) {
+        // Implementation
+    }
+
+    public static void move(Tekton tekton) {
+        //this.currentTekton = tekton;
+    }
+
+    public static void setIsInsect(boolean b) {
+        //this.isInsect = b;
+    }
+
+    public static void getIsInsect() {
+        //return isInsect;
+    }
+
+    public static void setCurrentTekton(Tekton t) {
+        //this.currentTekton = t;
+    }
+
+    public static void getCurrentTekton() {
+        // return currentTekton;
+    }
+
+    public static void setEffects(int[] effects) {
+        // Implementation
+    }
+
+    public static void getEffects() {
+        // return effects;
+    }
+
+    public static void setScore(int i) {
+        // this.score = i;
+    }
+
+    public static void getScore() {
+        // return score;
+    }
 }
