75dc273abebcd2eda1019aac45344e42481c25e1
darksidekovi <kovidavid96@gmail.com>
2025-03-20 00:14:49 +0100

    add basic input io

--- code/Proj_Lab/src/main/java/org/example/Main.java
+++ code/Proj_Lab/src/main/java/org/example/Main.java
@@ -1,7 +1,49 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.util.Scanner;
 
+public class Main {
+
+    // Add static variables here
+    public static Tekton T1;
+    // ...
+
+    public static void main(String[] args) {
+        int user_input = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        while (user_input >= 0) {
+            System.out.println("Tesztek leírása... -1: Exit\n");
+            System.out.println("Choose option: ");
+
+            try {
+                user_input = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Wrong input!");
+            }
+
+            switch(user_input) {
+                case 1: {
+                    callback1();
+                    break;
+                }
+                case 2: {
+                    // ...
+                    break;
+                }
+                // ...
+                default: {
+                    System.out.println("Unknown option.");
+                }
+            }
+            System.out.print("\n\n");
+        }
+
+        System.out.println("Goodbye...");
+    }
+
+    public static void callback1() {
+        System.out.println("Hello, I'm testing!");
     }
 }
