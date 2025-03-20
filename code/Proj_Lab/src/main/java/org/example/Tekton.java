72f29467ee12727c87eb6f4591a53232d158fddb
Herpai Levente <herpailevente2015@gmail.com>
2025-03-20 09:24:26 +0100

    Tekton class skeleton

--- code/Proj_Lab/src/main/java/org/example/Tekton.java
+++ code/Proj_Lab/src/main/java/org/example/Tekton.java
@@ -1,5 +1,81 @@
 package org.example;
+import java.util.List;
 
 public class Tekton {
+
+    //public List<Tekton> getNeighbours(){
+    public void getNeighbours(){
+        
+    }
+
+    public void addNeighbour(Tekton tekton){
+
+    }
+
+    public void removeNeighbour(Tekton tekton){
+
+    }
+
+    public Shroom getShroom(){
+        return new Shroom();
+    }
+
+    public void getSpores(){
+
+    }
+
+    public void getYarns(){
+
+    }
+
+    public TektonStrategy getStrategy(){
+        return new TektonStrategy() {
+            
+        };
+    }
+
+    public void setStrategy(TektonStrategy strategy){
+
+    }
+
+    public void addYarn(Yarn yarn){
+
+    }
+
+    public void removeYarn(Yarn yarn){
+
+    }
+
+    public void clearYarns(){
+
+    }
+
+    public void addShroom(Shroom shroom){
+
+    }
+
+    public void removeShroom(){
+        
+    }
+
+    public void addSpore(Spore spore){
+
+    }
+
+    public void removeSpore(Spore spore){
+
+    }
+
+    public void removeSomeSpore(List<Spore> spores){
+
+    }
+
+    public void split(){
+
+    }
+
+    public void doEffect(){
+
+    }    
 
 }
