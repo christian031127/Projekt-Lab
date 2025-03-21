0ca5053c5f6eb97b9699fe912f00b4bad2f36c73
molnrb <m.balazs3425@gmail.com>
2025-03-21 20:42:44 +0100

    Update Yarn.java

--- code/Proj_Lab/src/main/java/org/example/Yarn.java
+++ code/Proj_Lab/src/main/java/org/example/Yarn.java
@@ -1,5 +1,19 @@
 package org.example;
 
 public class Yarn {
+    public setTekton1(Tekton tekton1) {
 
+    }
+
+    public setTekton2(Tekton tekton2) {
+
+    }
+
+    public getTekton1() {
+
+    }
+
+    public getTekton2() {
+
+    }
 }
