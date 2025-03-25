4793c57d65ebb83fee1b1ce62a10c8786e2b0a8e
Spertli Krisztián <spertlik13@gmail.com>
2025-03-25 23:25:48 +0100

    vegso fasz#1

--- code/Proj_Lab/src/main/java/org/example/SlowingSpore.java
+++ code/Proj_Lab/src/main/java/org/example/SlowingSpore.java
@@ -4,6 +4,7 @@ public class SlowingSpore implements Spore {
 
     @Override
     public void addEffect(Player p){
-        System.out.println("SlowingSpore.addEffect(Player p) meghívva");
+        System.out.println("SlowingSpore.addEffect(Player p) called");
+        new Player().setEffects(new int[4]);
     }
 }
