449937610d1cfafe3722fa3f11105603a2fc02c6
Herpai Levente <herpailevente2015@gmail.com>
2025-03-28 18:03:09 +0100

    Teszt alapok

--- code/Proj_Lab/TestManager.sh
+++ code/Proj_Lab/TestManager.sh
@@ -7,6 +7,20 @@ mvn package
 
 echo "Első teszt futtatása"
 
-cat Tests/Test1/input.txt | mvn exec:java > Tests/Test1/output.txt
+osszTesztSzam=$(ls Tests | wc -l)
+echo "$osszTesztSzam teszteset található"
+
+
+#Összes teszt lefuttatása
+for file in Tests/*; do
+    echo "---- A következő teszt indul: $file ----"
+    cat $file/input.txt
+    #cat $file/input.txt | java -jar target/Proj_Lab-1.0-SNAPSHOT.jar > $file/output.txt
+    cat $file/input.txt | mvn exec:java > $file/output.txt
+
+    #Eredmény ellenőrzése
+done
+
+#cat Tests/Test1/input.txt | mvn exec:java > Tests/Test1/output.txt
 
 echo "Kész"
