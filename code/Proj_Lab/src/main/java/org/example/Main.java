02ea61de0c35a3d48a830a8c4a0de96cf627c87b
Leventa <levente@gmai.com>
2025-04-03 13:18:18 +0200

    Main átalakítása, bemutatott tesztesetek kitörlése. Tesztelő környezet parancsainak feldolgozásának kezdete

--- code/Proj_Lab/src/main/java/org/example/Main.java
+++ code/Proj_Lab/src/main/java/org/example/Main.java
@@ -12,82 +12,51 @@ public class Main {
     // ...
 
     public static void main(String[] args) {
-        int user_input = 0;
 
-        while (user_input >= 0) {
-            System.out.println("Tesztek leírása... -1: Exit\n");
-            System.out.println("1. Fonál növesztés"); //2opciót felajánl: másik tektonra útként/ tektonra rá
-            System.out.println("2. Fonál elrágás");
-            System.out.println("3. Fonál felszívódás");
-            System.out.println("4. Gomba növesztés spórából");
-            System.out.println("5. Spóra szórás gombával");
-            System.out.println("6. Rovar mozgás");
-            System.out.println("7. Spóra elfogyasztása rovarral"); //eset utána
-            System.out.println("8. Gomba halál");
-            System.out.println("9. Tekton kettéválás");
-            System.out.println("Choose option: ");
+        String sorBe = "";
+        String [] command = null;
 
-            try {
-                user_input = Integer.parseInt(scanner.nextLine());
-            } catch (NumberFormatException e) {
-                System.out.println("Wrong input!");
-                continue;
-            }
+        System.out.println("Az exit paranccsal lehet kilépni");
+        while(!(sorBe = scanner.nextLine().split(",")).equals("exit"){
 
-            switch (user_input) {
-                case 1: {
-                    new Player().interactWithYarn(new Yarn());
-                    System.out.println("Teszt sikeresen lefutott.");
+            System.out.println("soros" + sorBe);
+            command = sorBe.split(",");
+
+            switch(command[0]){
+                case "add": {
+
+                    break;   
+                }
+
+                case "neighbour": {
+
                     break;
                 }
-                case 2: {
-                    new Player().getCurrentTekton().getYarns();
-                    new Player().interactWithYarn(new Yarn());
-                    System.out.println("Teszt sikeresen lefutott.");
-                    break;
+                case "yarn": {
+                
+                    break;     
+                } 
+                case "remove-yarn": {
+
+                    break;   
                 }
-                case 3: {
-                    new Tekton().doEffect();
-                    System.out.println("Teszt sikeresen lefutott.");
-                    break;
+                case "split": {
+
+                    break;   
+                }   
+                case "move": {
+
+                    break;      
                 }
-                case 4: {
-                    new Player().getCurrentTekton().getSpores();
-                    new Player().interactWithSpore(new ArrayList<Spore>());
-                    System.out.println("Teszt sikeresen lefutott.");
-                    break;
+                case "eat": {
+
+                    break;      
                 }
-                case 5: {
-                    new Player().move(new Tekton());
-                    System.out.println("Teszt sikeresen lefutott.");
-                    break;
-                }
-                case 6: {
-                    new Player().move(new Tekton());
-                    System.out.println("Teszt sikeresen lefutott.");
-                    break;
-                }
-                case 7: {
-                    new Player().interactWithSpore(new Tekton().getSpores());
-                    System.out.println("Teszt sikeresen lefutott.");
-                    break;
-                }
-                case 8: {
-                    new Shroom().age();
-                    System.out.println("Teszt sikeresen lefutott.");
-                    break;
-                }
-                case 9: {
-                    new Map().splitTekton(new Tekton());
-                    System.out.println("Teszt sikeresen lefutott.");
-                    break;
-                }
-                // ...
-                default: {
-                    System.out.println("Unknown option.");
+                case default:{
+                    //HELP
+                    break;      
                 }
             }
-            System.out.print("\n\n");
         }
         System.out.println("Goodbye...");
     }
