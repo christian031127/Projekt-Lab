4cfe65596be6bced9fd219f819cfcec7014a0ab3
Herpai Levente <herpailevente2015@gmail.com>
2025-04-16 18:47:23 +0200

    Javított TestManager és alap tesztek kijatítása

--- code/Proj_Lab/TestManager.bat
+++ code/Proj_Lab/TestManager.bat
@@ -1,52 +1,60 @@
 @echo off
 setlocal enabledelayedexpansion
 
-rem Színek beállítása (Windows nem támogatja a színes kimenetet alapból)
+echo Project buildelese
+call .\mvnw.cmd compile
+echo Project csomagolasa
+call .\mvnw.cmd package
 
-echo Project buildelése
-mvn compile
-echo Project csomagolása
-mvn package
-
-rem Tesztfájlok megszámolása
-set /a osszTesztSzam=0
-for %%F in (Tests\*) do set /a osszTesztSzam+=1
-
-echo %osszTesztSzam% teszteset található
 set /a sikeresTesztek=0
+set /a osszTesztSzam=0
 
-rem Összes teszt lefuttatása
-for %%F in (Tests\*) do (
-    echo ---- A következő teszt indul: %%F ----
-    
-    rem Input futtatása és output mentése
-    type %%F\input.txt | mvn exec:java > %%F\output.txt
-
-    rem Eredmény ellenőrzése
-    if exist %%F\output.txt (
-        findstr /C:"BUILD SUCCESS" %%F\output.txt >nul
-        if !errorlevel! == 0 (
-            set /a sikeresTesztek+=1
-            echo %%F lefutása SIKERES!
-        ) else (
-            echo %%F lefutása SIKERTELEN!
-            echo Részletek:
-            findstr /C:"ERROR" %%F\output.txt
-        )
-    ) else (
-        echo Nem található output fájl.
-    )
-
-    echo ---- %%F Vége ----
+for /d %%G in (Tests\*) do (
+    set /a osszTesztSzam+=1
 )
 
-echo Tesztek futtatásának vége!
-echo Eredmények: %sikeresTesztek% / %osszTesztSzam% (Sikeres / Sikertelen tesztek)
+echo %osszTesztSzam% teszteset talalhato
 
-if %sikeresTesztek% == %osszTesztSzam% (
-    echo ÖSSZES TESZT SIKERES!
+for /d %%F in (Tests\*) do (
+    echo ---- A kovetkezo teszt indul: %%F ----
+
+    type "%%F\input.txt" | java -cp .\target\Proj_Lab-1.0-SNAPSHOT.jar org.example.Teszt > "%%F\output.txt"
+    type logfile.log >> "%%F\output.txt"
+
+    if exist "%%F\output.txt" (
+        set sikeres=1
+
+        for /f "usebackq delims=" %%A in ("%%F\expected.txt") do (
+            findstr /c:"%%A" "%%F\output.txt" >nul
+            if errorlevel 1 (
+                echo Nem talalhato: %%A
+                set sikeres=0
+            )
+        )
+
+        if "!sikeres!"=="1" (
+            set /a sikeresTesztek+=1
+            echo %%F lefutasa SIKERES!
+        ) else (
+            echo %%F lefutasa SIKERTELEN!
+            echo Reszletek:
+            findstr /r /c:"ERROR" /c:"WARNING" "%%F\output.txt"
+        )
+    ) else (
+        echo nem talalhato fajl
+    )
+
+    echo ---- %%F Vege ----
+)
+
+echo Tesztek futtatasanak vege!
+echo Eredmenyek: %sikeresTesztek% / %osszTesztSzam% (Sikeres / sikertelen tesztek)
+
+if "%sikeresTesztek%"=="%osszTesztSzam%" (
+    echo OSSZES TESZT SIKERES!
 ) else (
-    echo SIKERTELEN, van még mit javítani!
+    echo SIKERTELEN, van meg mit javitani!
 )
 
 endlocal
+pause
