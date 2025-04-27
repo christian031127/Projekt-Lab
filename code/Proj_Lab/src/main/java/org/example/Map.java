35f3a99ba10ac5b38b07494e0fb18862f749602b
Molnár Dávid <lerinter055@gmail.com>
2025-04-27 20:33:42 +0200

    Map, Yarn

--- code/Proj_Lab/src/main/java/org/example/Map.java
+++ code/Proj_Lab/src/main/java/org/example/Map.java
@@ -1,17 +1,153 @@
 package org.example;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.Scanner;
+import java.util.List;
+import static org.example.Main.logger;
+
 public class Map {
 
-    //private Tektons;
-    //private Players;
-    //public currentPlayer;
+
+    public Player currentPlayer;
+    private static HashMap<String, Object> Tektons = new HashMap<String,Object>();
+    private static HashMap<String, Object> Players= new HashMap<String, Object>();
+    static String Name;
+
+
     public static int currentTurn = 0;
 
-    public void loadMap(){
+
+    public void loadMap() throws Exception {
         // read file, load map
+        Scanner scanner = null;
+        try{
+            File file = new File("map.txt");
+            scanner = new Scanner(file);
+        }
+        catch (FileNotFoundException e) {
+            logger.log(Level.SEVERE, "Cannot find Map");
+        }
+        String [] command = null;
+        while(scanner.hasNextLine()){
+
+            String line = scanner.nextLine();
+            //Kommente figyelmen kívül hagyása
+            if(line.contains("#") || line.contains("//")) {
+                continue;
+            }
+            command=line.split(" ");
+
+            switch(command[0]){
+                case "add": {
+                    Object uj = new Object();
+                    //Van e elég argumentum?
+                    if(command.length < 3){
+                        logger.log(Level.SEVERE, "Not enough arguments for 'add' command! (minimum 2 argument)");
+                        throw new Exception("Not enough arguments for 'add' command! (minimum 2 argument)");
+                    }
+                    if(command.length == 3){
+                        switch (command[1]) {
+                            case "Tekton":
+                                uj = new Tekton();
+                                Tekton uj2 = (Tekton)uj;
+                                uj2.setStrategy(new MultipleYarnTekton());
+                                Name=command[2];
+                                Tektons.put(Name, uj);
+                                break;
+
+                            default:
+                                logger.log(Level.SEVERE, "Cannot find gameObject type {0}!", command[1]);
+                                throw new AssertionError();
+                        }
+                    }
+                    if(command.length > 3){
+                        switch (command[1]) {
+                            case "Tekton":
+                                //Ez a sor lehet nem azt csinálja amit kellene
+                                uj = new Tekton();
+                                Tekton uj2 = (Tekton)uj;
+                                switch (command[3]) {
+                                    case "AbsorbTekton":
+                                        uj2= new Tekton();
+                                        uj2.setStrategy(new AbsorbTekton());
+                                        Name=command[2];
+                                        break;
+                                    case "SingleYarnTekton":
+                                        uj2= new Tekton();
+                                        uj2.setStrategy(new SingleYarnTekton());
+                                        Name=command[2];
+                                        break;
+                                    case "NonShroomTekton":
+                                        uj2= new Tekton();
+                                        uj2.setStrategy(new NonShroomTekton());
+                                        Name=command[2];
+                                        break;
+                                    case "KeepAliveTekton":
+                                        uj2= new Tekton();
+                                        uj2.setStrategy(new KeepAliveTekton());
+                                        Name=command[2];
+                                        break;
+                                    default:
+
+                                        throw new AssertionError();
+                                }
+                                Tektons.put(Name, uj);
+                                break;
+
+                            case "Player":
+                                if (command.length != 5) {
+                                    throw new Exception("add Player takes 3 argument!");
+                                }
+
+
+                                Player p1 = new Player();
+                                p1.setIsInsect(command[4].equals("Insect"));
+                                Tekton t1 = (Tekton) Tektons.get(command[3]);
+                                p1.setCurrentTekton(t1);
+
+                                uj = p1;
+                                Name = command[2];
+                                Players.put(Name, uj);
+                                break;
+                            default:
+                                logger.log(Level.SEVERE, "Cannot find gameObject type {0}!", command[1]);
+                                throw new AssertionError();
+                        }
+                    }
+
+                    logger.log(Level.INFO, "Adding new gameObject {0}!", command[2]);
+                    break;
+                }
+
+                case "neighbour": {
+                    if(command.length != 3){
+                        logger.log(Level.SEVERE, "Command neighbour takes 2 arguments!");
+                        throw new Exception("Command neighbour takes 2 arguments!");
+                    }
+                    Tekton elso = (Tekton)Tektons.get(command[1]);
+                    Tekton masodik = (Tekton)Tektons.get(command[2]);
+
+                    elso.addNeighbour((Tekton)masodik);
+                    masodik.addNeighbour((Tekton)elso);
+                    logger.log(Level.INFO, "Tekton " + command[1] + " Tekton " + command[2] + " are now neighbours!");
+                    break;
+                }
+
+                default:{
+                    //HELP
+                    logger.log(Level.WARNING, "Command {0} not found!", command[0]);
+                    break;
+                }
+            }
+        }
+
     }
     public void splitTekton(Tekton tekton){
-        System.out.println("Map.splitTekton() called");
+        logger.log(Level.FINE,"Map.splitTekton() called");
         tekton.split();
     }
 }
--- code/Proj_Lab/src/main/java/org/example/Yarn.java
+++ code/Proj_Lab/src/main/java/org/example/Yarn.java
@@ -1,41 +1,59 @@
 package org.example;
 
+import java.util.logging.Level;
+
+import static org.example.Main.logger;
+
 public class Yarn {
 
-    // private Tekton T1;
-    // private Tekton T2;
-    // private int shroom_player_id;
+    private Tekton T1;
+    private Tekton T2;
+    private int shroom_player_id;
+
     public void setTekton1(Tekton tekton1) {
         if(tekton1==null){
-            new Tekton().removeYarn(this);
+            T1.removeYarn(this);
+            T1=null;
         }
         else{
-            tekton1.addYarn(this);
+            T1 = tekton1;
+            T1.addYarn(this);
         }
 
-        System.out.println("Yarn.setTekton1() called");
+        logger.log(Level.FINE,"Yarn.setTekton1() called");
     }
 
     public void setTekton2(Tekton tekton2) {
         if(tekton2==null){
-            new Tekton().removeYarn(this);
+            T2.removeYarn(this);
         }
         else{
-            tekton2.addYarn(this);
+            T2 = tekton2;
+            T2.addYarn(this);
         }
-        System.out.println("Yarn.setTekton2() called");
+        logger.log(Level.FINE,"Yarn.setTekton2() called");
     }
 
     public Tekton getTekton1() {
-        return new Tekton();
+        return T1;
     }
 
     public Tekton getTekton2() {
-        return new Tekton();
+        return T2;
     }
 
+    public void setShroom_player_id(int shroom_player_id) {
+        this.shroom_player_id = shroom_player_id;
+        logger.log(Level.FINE,"Yarn.setShroom_player_id() called");
+    }
+    public int getShroom_player_id() {
+        return shroom_player_id;
+    }
     public boolean isSingleTektonYarn() {
-        return true;
+        if(T1!=null && T2==null){
+            return true;
+        }
+        else return false;
     }
 
     public int getShroomPlayerId() {
