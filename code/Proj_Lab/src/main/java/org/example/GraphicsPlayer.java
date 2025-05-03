8dcac9301587843472236c35912024e047f755b6
darksidekovi <kovidavid96@gmail.com>
2025-05-03 20:58:27 +0200

    graphics object skeleton

---
+++ code/Proj_Lab/src/main/java/org/example/GraphicsPlayer.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.awt.*;
+
+public class GraphicsPlayer {
+
+    private Player player;
+    public int x;
+    public int y;
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void draw(Graphics2D g) {}
+}
---
+++ code/Proj_Lab/src/main/java/org/example/GraphicsTekton.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.awt.*;
+
+public class GraphicsTekton {
+
+    private Tekton tekton;
+    public int x;
+    public int y;
+
+    public Tekton getTekton() {
+        return tekton;
+    }
+
+    public void draw(Graphics2D g) {}
+}
